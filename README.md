816c548059072ebcbc7ce4f64e8839207279f352
Koe-eigh <gongeizhitian@gmail.com>
2025-01-26 13:50:14 +0900

    開発環境setup用にREADMEとseed dataを追加

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # shiftplanner
+学習塾の講師のシフトを最適化するアプリケーションです。
+
+## 開発環境のセットアップ
+
+```bash
+cp .env.example .env
+```
+
+```bash
+docker compose up -d
+```
---
+++ db/docker-entrypoint-initdb.d/1-mongo-init.js
@@ -0,0 +1,20 @@
+let solver = {
+    user: "solver",
+    pwd: "password",
+    roles: [
+        {
+            role: "dbOwner",
+            db: "shiftplanner_solver"
+        }
+    ]
+};
+
+db.createUser(solver);
+db.createCollection("role");
+db.createCollection("user");
+db.createCollection("user_to_student");
+db.createCollection("students");
+db.createCollection("user_to_teacher");
+db.createCollection("teachers");
+db.createCollection("timetables");
+db.createCollection("user_to_timetable");
